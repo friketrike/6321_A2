8c5fcdba248331142eb290cd21519b4aea020119
friketrike <friketrike@gmail.com>
2016-10-12 03:27:50 -0400

    question 1 is shaping up...

--- A2_driver.m
+++ A2_driver.m
@@ -5,20 +5,14 @@ phi = load('hw2x.dat');
 phi = [phi, ones(size(phi,1),1)];
 y = load('hw2y.dat');
 
-figure(1)
-plot(phi(:,1), y, '.');
-hold on;
-plot(phi(:,2), y, '.r');
-plot(phi(:,3), y, '.k');
-title('Confetti')
-hold off
-
 % Partition the data randomly
 % idx_test = [51, 29, 87, 93, 59, 1, 90, 23, 39, 96]; looks nice on the
 % plots... bad doctoring of data, slap on the hand...
 idxs = randperm(size(phi, 1));
-idx_training = idxs(1:89);
-idx_test = idxs(90:99);
+% idx_training = idxs(1:89);
+idx_training = load('idx_train.dat');
+% idx_test = idxs(90:99);
+idx_test = load('idx_test.dat');
 
 phi_training = phi(idx_training, :);
 y_training = y(idx_training);
@@ -42,7 +36,7 @@ for lambda = lambdas
 end
 
 % Now plot
-figure(2); % RMS train / test vs lambda
+figure(1); % RMS train / test vs lambda
 subplot(2,2,1)
 plot(lambdas(1:35), j_h_training(1:35), '.k')
 title('RMS L2 \lambda < 40')
@@ -63,18 +57,20 @@ plot(lambdas, j_h_test, '.g')
 legend('training set', 'test set')
 hold off
 
-%figure(3); % Weights w_{L2} as a function of lambda
+% Weights w_{L2} as a function of lambda
 subplot(2,2,3:4)
 plot(lambdas, w(:,1), '.k')
-title('w_n{L2} as a function of \lambda')
+title('w_{n}L2 as a function of \lambda')
 xlabel('\lambda')
 hold on
 plot(lambdas, w(:,2), '.g')
 plot(lambdas, w(:,3), '.r')
 plot(lambdas, w(:,4), '.')
-legend('w1_{L2}', 'w2_{L2}', 'w3_{L2}', 'w4_{L2}')
+legend('w_1{L2}', 'w_2{L2}', 'w_3{L2}', 'w_4{L2}')
 axis([0, 3500, -0.5, 0.17])
 hold off
+
+print test.pdf
 
 w_quad = zeros(length(lambdas), size(phi,2));
 for lambda = lambdas
@@ -91,8 +87,8 @@ for lambda = lambdas
   j_h_test_quad(idx) = rms(h_phi_test_quad(:, idx) - y_test);
 end
 
-% Now plot
-figure(3); % RMS train / test vs lambda
+% Now plot L1
+figure(2); % RMS train / test vs lambda
 subplot(2,1,1)
 plot(lambdas(1:25), j_h_training_quad(1:25), '.k')
 title('RMS error for L1 regularization')
@@ -112,7 +108,8 @@ plot(lambdas(1:50), j_h_test_quad(1:50), '.g')
 legend('training set', 'test set')
 hold off
 
-figure(4); % Weights as a function of lambda
+% Weights as a function of lambda
+figure(4); 
 subplot(2,2,1)
 title('w_{L1} as a function of \lambda')
 plot(lambdas, w_quad(:,2), 'xg')
@@ -139,6 +136,3 @@ plot(lambdas(1:35), w_quad(1:35,3), '.r')
 plot(lambdas(1:35), w_quad(1:35,4), '.')
 legend('w_1{L1}', 'w_2{L1}', 'w_3{L1}','w_4{L1}')
 hold off
-
-
-
