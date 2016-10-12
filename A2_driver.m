0462d19ff484dce34fb90bb02873592474c1228e
friketrike <friketrike@gmail.com>
2016-10-11 23:37:25 -0400

    advances during the lab

--- A2_driver.m
+++ A2_driver.m
@@ -14,6 +14,8 @@ title('Confetti')
 hold off
 
 % Partition the data randomly
+% idx_test = [51, 29, 87, 93, 59, 1, 90, 23, 39, 96]; looks nice on the
+% plots... bad doctoring of data, slap on the hand...
 idxs = randperm(size(phi, 1));
 idx_training = idxs(1:89);
 idx_test = idxs(90:99);
@@ -23,7 +25,7 @@ y_training = y(idx_training);
 phi_test = phi(idx_test, :);
 y_test = y(idx_test);
 
-lambdas = 0:0.1:15;
+lambdas = 0:0.1:24;
 lambdas = lambdas .^ 3;
 w = zeros(length(lambdas), size(phi,2));
 for lambda = lambdas
@@ -33,18 +35,27 @@ for lambda = lambdas
                 + (lambda * eye(size(phi_training, 2)))) ...
                 * (phi_training' * y_training);
   h_phi_training(:, idx) = phi_training * w(idx, :)';
-  j_h_training(idx) = (sum((h_phi_training(:, idx) - y_training).^2) ...
-                            / (2*size(phi_training,1))).^0.5;
+  j_h_training(idx) = rms(h_phi_training(:, idx) - y_training);
   % Now compare to the test
   h_phi_test(:, idx) = phi_test * w(idx, :)';
-  j_h_test(idx) = (sum((h_phi_test(:, idx) - y_test).^2) ...
-                            / (2*size(phi_test,1))).^0.5;                          
+  j_h_test(idx) = rms(h_phi_test(:, idx) - y_test);                          
 end
 
 % Now plot
 figure(2); % RMS train / test vs lambda
+subplot(2,2,1)
+plot(lambdas(1:35), j_h_training(1:35), '.k')
+title('RMS L2 \lambda < 40')
+xlabel('\lambda')
+ylabel('\epsilon_{L2} RMS')
+hold on
+plot(lambdas(1:35), j_h_test(1:35), '.g')
+legend('training set', 'test set')
+hold off
+
+subplot(2,2,2)
 plot(lambdas, j_h_training, '.k')
-title('RMS error for L2 regularization')
+title('RMS L2 \lambda')
 xlabel('\lambda')
 ylabel('\epsilon_{L2} RMS')
 hold on
@@ -52,9 +63,10 @@ plot(lambdas, j_h_test, '.g')
 legend('training set', 'test set')
 hold off
 
-figure(3); % Weights w_{L2} as a function of lambda
+%figure(3); % Weights w_{L2} as a function of lambda
+subplot(2,2,3:4)
 plot(lambdas, w(:,1), '.k')
-title('w_{L2} as a function of \lambda')
+title('w_n{L2} as a function of \lambda')
 xlabel('\lambda')
 hold on
 plot(lambdas, w(:,2), '.g')
@@ -68,36 +80,65 @@ w_quad = zeros(length(lambdas), size(phi,2));
 for lambda = lambdas
   idx = lambda == lambdas;
   w_quad(idx,:) = quadprog(2*(phi_training'*phi_training), ...
-                      2*(y_training'*phi_training), ...
+                      -2*(phi_training'*y_training), ...
                       lambda*[1,1,1,0;1,1,-1,0;1,-1,1,0;1,-1,-1,0;...
                               -1,1,1,0;-1,1,-1,0;-1,-1,1,0;-1,-1,-1,0],... 
                       [1;1;1;1;1;1;1;1]);
   h_phi_training_quad(:, idx) = phi_training * w_quad(idx, :)';
-  j_h_training_quad(idx) = (sum((h_phi_training_quad(:, idx) - y_training).^2) ...
-                            / (2*size(phi_training,1))).^0.5;
+  j_h_training_quad(idx) = rms(h_phi_training_quad(:, idx) - y_training);
   % Now compare to the test
   h_phi_test_quad(:, idx) = phi_test * w_quad(idx, :)';
-  j_h_test_quad(idx) = (sum((h_phi_test(:, idx) - y_test).^2) ...
-                            / (2*size(phi_test,1))).^0.5;
+  j_h_test_quad(idx) = rms(h_phi_test_quad(:, idx) - y_test);
 end
 
 % Now plot
-figure(4); % RMS train / test vs lambda
-plot(lambdas, j_h_training_quad, '.k')
+figure(3); % RMS train / test vs lambda
+subplot(2,1,1)
+plot(lambdas(1:25), j_h_training_quad(1:25), '.k')
 title('RMS error for L1 regularization')
 xlabel('\lambda')
 ylabel('\epsilon_{L1} RMS')
 hold on
-plot(lambdas, j_h_test_quad, '.g')
+plot(lambdas(1:25), j_h_test_quad(1:25), '.g')
+legend('training set', 'test set')
+hold off
+subplot(2,1,2)
+plot(lambdas(1:50), j_h_training_quad(1:50), '.k')
+title('RMS error for L1 regularization')
+xlabel('\lambda')
+ylabel('\epsilon_{L1} RMS')
+hold on
+plot(lambdas(1:50), j_h_test_quad(1:50), '.g')
 legend('training set', 'test set')
 hold off
 
-figure(5); % Weights as a function of lambda
-plot(lambdas, w_quad(:,1), '.k')
+figure(4); % Weights as a function of lambda
+subplot(2,2,1)
+title('w_{L1} as a function of \lambda')
+plot(lambdas, w_quad(:,2), 'xg')
+hold on
+plot(lambdas, w_quad(:,3), '.r')
+legend('w_2{L1}', 'w_3{L1}')
+axis([0, 3, -0.05, 0.05])
+hold off
+subplot(2,2,2)
+plot(lambdas(1:23), w_quad(1:23,1), '.k')
 title('w_{L1} as a function of \lambda')
 hold on
-plot(lambdas, w_quad(:,2), 'xg')
-plot(lambdas, w_quad(:,3), '.r')
-legend('w_1{L1}', 'w_2{L1}', 'w_3{L1}')
-axis([0, 400, -0.04, 0.1])
+plot(lambdas(1:23), w_quad(1:23,2), 'xg')
+plot(lambdas(1:23), w_quad(1:23,3), '.r')
+plot(lambdas(1:23), w_quad(1:23,4), '.')
+legend('w_1{L1}', 'w_2{L1}', 'w_3{L1}','w_4{L1}')
 hold off
+subplot(2,2,3:4)
+plot(lambdas(1:35), w_quad(1:35,1), '.k')
+title('Trend of all coefficients')
+hold on
+plot(lambdas(1:35), w_quad(1:35,2), 'xg')
+plot(lambdas(1:35), w_quad(1:35,3), '.r')
+plot(lambdas(1:35), w_quad(1:35,4), '.')
+legend('w_1{L1}', 'w_2{L1}', 'w_3{L1}','w_4{L1}')
+hold off
+
+
+
